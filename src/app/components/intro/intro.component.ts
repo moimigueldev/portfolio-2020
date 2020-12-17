11eb7106fcbcd3278d6aff2f5e230f633fe19d69
moises <moimigueldev@gmail.com>
2020-12-17 17:35:43 -0600

    created intro

---
+++ src/app/components/intro/intro.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+declare var VANTA;
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.scss']
+})
+export class IntroComponent implements OnInit {
+
+  constructor() { }
+
+  animate = false;
+  cta = false
+
+  ngOnInit(): void {
+
+    // set dynamic globe background
+    VANTA.GLOBE({
+      el: "#intro",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00
+    })
+
+    // animate heading content
+    setTimeout(() => this.animate = true, 1000)
+
+    // set timeout to animate scroll down call to action
+    setTimeout(() => this.cta = true, 3000)
+
+
+
+
+
+
+
+  }
+
+}
